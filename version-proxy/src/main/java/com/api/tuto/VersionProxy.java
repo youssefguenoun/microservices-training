bcd61f94001c14a7e72b255046d7862bd74d8f85
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-06-27 17:11:21 +0200

    step08 implementing versionning

---
+++ version-proxy/src/main/java/com/api/tuto/VersionProxy.java
@@ -0,0 +1,13 @@
+package com.api.tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionProxy {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionProxy.class, args);
+    }
+
+}
