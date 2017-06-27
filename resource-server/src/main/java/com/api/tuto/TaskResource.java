ec181f623081d9e884b380ca38ad38cce70737e1
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-06-27 16:19:38 +0200

    step2-solution api backend implementation

--- resource-server/src/main/java/com/api/tuto/TaskResource.java
+++ resource-server/src/main/java/com/api/tuto/TaskResource.java
@@ -4,14 +4,24 @@ package com.api.tuto;
 import com.api.tuto.domain.Task;
 import com.api.tuto.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class TaskResource {
 
     @Autowired
     private TaskRepository taskRepository;
 
+    @Transactional(readOnly = true)
+    @RequestMapping(path="/tasks", method = RequestMethod.GET)
+    @CrossOrigin(origins ="http://editor.swagger.io")
     List<Task> getTasks()  {
         List<Task> tasks = taskRepository.findAll();
 
