33629a8c11e2a41d6e197e5cecb7ed86c9058972
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-06-27 17:12:32 +0200

    step08-solution implementing versionning

--- version-proxy/src/main/java/com/api/tuto/VersionProxy.java
+++ version-proxy/src/main/java/com/api/tuto/VersionProxy.java
@@ -2,8 +2,10 @@ package com.api.tuto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class VersionProxy {
 
     public static void main(String[] args) {
