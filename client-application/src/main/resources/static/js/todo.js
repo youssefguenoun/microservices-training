b7afb570468886751964772d96c3324405a50382
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-06-28 16:29:06 +0200

    step11 implementing implicit flow

---
+++ client-application/src/main/resources/static/js/todo.js
@@ -0,0 +1,39 @@
+angular.module('hello', ['ngRoute', 'afOAuth2']).config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'todo.html',
+		controller : 'todo',
+		controllerAs : 'controller'
+	}).otherwise('/');
+}).controller('navigation',
+
+function($rootScope, $http, $location, $route) {
+
+	var self = this;
+
+	self.tab = function(route) {
+		return $route.current && route === $route.current.controller;
+	};
+
+	$rootScope.authenticated = false;
+	$http.get('user').then(function(response) {
+		if (response.data.name) {
+			$rootScope.authenticated = true;
+			$rootScope.userName = response.data.name;
+		} 
+	}, function(response) {
+		console.log(response);
+	});
+
+	self.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootScope.authenticated = false;
+			$rootScope.userName = "";
+			$location.path("/");
+		});
+	}
+}).controller('todo', function($http) {
+	var self = this;
+	$http.get('/api/tasks').then(function(response) {
+		self.todos = response.data;
+	})
+});
