6b6bffb468003d20cf1d146358215697843095ba
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-07-02 20:46:05 +0200

    step 16-solution implementating circuit breaker turbine

--- online-store/support/turbine-server/src/main/java/com/sfeir/school/microservices/turbineserver/TurbineServerApplication.java
+++ online-store/support/turbine-server/src/main/java/com/sfeir/school/microservices/turbineserver/TurbineServerApplication.java
@@ -2,8 +2,12 @@ package com.sfeir.school.microservices.turbineserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
