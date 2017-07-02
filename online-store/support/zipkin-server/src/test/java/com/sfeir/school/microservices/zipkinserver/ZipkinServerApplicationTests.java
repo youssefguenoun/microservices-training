4881a2803b17a89c363dd097f435711f3971dcf6
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-07-02 20:48:51 +0200

    step 17 implementing distributed tracing

--- online-store/support/zipkin-server/src/test/java/com/sfeir/school/microservices/zipkinserver/ZipkinServerApplicationTests.java
+++ online-store/support/zipkin-server/src/test/java/com/sfeir/school/microservices/zipkinserver/ZipkinServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sfeir.school.microservices.turbineserver;
+package com.sfeir.school.microservices.zipkinserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TurbineServerApplicationTests {
+public class ZipkinServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
