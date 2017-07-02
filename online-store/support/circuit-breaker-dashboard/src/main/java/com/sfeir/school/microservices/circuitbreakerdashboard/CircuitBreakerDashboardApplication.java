5021daf4ac244c4f2caafb9234e6db13f6f48030
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-07-02 11:56:37 +0200

    step 15 implementating circuit breaker hystrix

---
+++ online-store/support/circuit-breaker-dashboard/src/main/java/com/sfeir/school/microservices/circuitbreakerdashboard/CircuitBreakerDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.sfeir.school.microservices.circuitbreakerdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerDashboardApplication.class, args);
+	}
+}
