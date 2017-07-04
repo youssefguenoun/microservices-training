8f31f11701d4f2d7c14c636ac634c695df84bb92
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-07-04 17:50:35 +0200

    implementing step18-solution edge server

--- online-store/support/edge-server/src/main/java/com/sfeir/school/microservice/support/edge/ZuulApplication.java
+++ online-store/support/edge-server/src/main/java/com/sfeir/school/microservice/support/edge/ZuulApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
