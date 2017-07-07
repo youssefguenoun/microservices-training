6ce637a00cc63c7de56b98912ee2988e1caf11a9
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-07-07 10:56:01 +0200

    Fix step17-solution distributed tracing zipkin

--- online-store/support/zipkin-server/src/main/java/com/sfeir/school/microservices/zipkinserver/ZipkinServerApplication.java
+++ online-store/support/zipkin-server/src/main/java/com/sfeir/school/microservices/zipkinserver/ZipkinServerApplication.java
@@ -2,8 +2,10 @@ package com.sfeir.school.microservices.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
